6179e49f4118e99cff53e0dc03294466b25c2362
Giannis Papadakis <gpapadakis@gwi.com>
2023-12-18 08:38:49 +0200

    develop: Clean environment

--- src/main/java/com/prometheus/selenium/PrometheusListeners.java
+++ src/main/java/com/prometheus/selenium/PrometheusListeners.java
@@ -1,48 +1,10 @@
 package com.prometheus.selenium;
 
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Gauge;
-import io.prometheus.client.exporter.PushGateway;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class PrometheusListeners implements ITestListener {
-
-    List<String> labelKeys = Arrays.asList("author");
-    List<String> labelValues = Arrays.asList("gpapadakis");
-
-    PushGateway client = new PushGateway("localhost:9091");
-    CollectorRegistry registry = CollectorRegistry.defaultRegistry;
-
-    String jobName = "selenium";
-    String passed = "TestPassed";
-    String failed = "TestFailed";
-    String skipped = "TestSkipped";
-    String help = "metric_help";
-
-    Gauge passedTests = Gauge.build()
-            .name(passed)
-            .help(help)
-            .labelNames(labelKeys.toArray(new String[0]))
-            .register(registry);
-
-    Gauge failedTests = Gauge.build()
-            .name(failed)
-            .help(help)
-            .labelNames(labelKeys.toArray(new String[0]))
-            .register(registry);
-
-    Gauge skippedTests = Gauge.build()
-            .name(skipped)
-            .help(help)
-            .labelNames(labelKeys.toArray(new String[0]))
-            .register(registry);
 
     @Override
     public void onTestStart(ITestResult result) {
@@ -50,36 +12,16 @@ public class PrometheusListeners implements ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult result) {
-
-        passedTests.labels(labelValues.toArray(new String[0])).inc();
-
-        try {
-            client.push(registry, jobName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        failedTests.labels(labelValues.toArray(new String[0])).inc();
 
-        try {
-            client.push(registry, jobName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        skippedTests.labels(labelValues.toArray(new String[0])).inc();
 
-        try {
-            client.push(registry, jobName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -88,7 +30,6 @@ public class PrometheusListeners implements ITestListener {
 
     @Override
     public void onTestFailedWithTimeout(ITestResult result) {
-        this.onTestFailure(result);
     }
 
     @Override
